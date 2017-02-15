57f9af21473e85c29f1463adb297abfbca4143c5
Thomas Stalcup Jr <tstalcupjr@gmail.com>
2017-02-15 16:58:44 -0500

    Adds Test STub

---
+++ test/test-server.js
@@ -0,0 +1,6 @@
+describe('Shopping List', function() {
+    it('should list items on get');
+    it('should add an item on post');
+    it('should edit an item on put');
+    it('should delete an item on delete');
+});
